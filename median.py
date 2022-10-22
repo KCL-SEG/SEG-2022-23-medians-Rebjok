df6c2bb3cedebfbfc7be22b530c7cb4f362e3dc0
Rebi.boy <rebjok911@gmail.com>
2022-10-22 15:04:03 +0100

    Thrid try

--- median.py
+++ median.py
@@ -8,15 +8,16 @@ while True:
         numbers.sort()
         if len(numbers)%2 == 1:
             index = int((len(numbers)-1)/2)
-            print(numbers[index])
+            median =  numbers[index]
         elif len(numbers)%2==0:
             index = int(len(numbers)/2)
             num1 = numbers[index]
             num2 = numbers[index-1]
-            print((num1+num2)/2)
+            median =  (num1+num2)/2
 
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
 print(numbers)
+print(median)
