d8613b84a32ec312975f025547f1250ffc1c8a09
Rebi.boy <rebjok911@gmail.com>
2022-10-22 14:56:49 +0100

    second try

--- median.py
+++ median.py
@@ -7,10 +7,12 @@ while True:
         numbers = [float(value) for value in input().split(",")]
         numbers.sort()
         if len(numbers)%2 == 1:
-            print(numbers[(len(numbers)-1)/2])
+            index = int((len(numbers)-1)/2)
+            print(numbers[index])
         elif len(numbers)%2==0:
-            num1 = numbers[len(numbers)/2]
-            num2 = numbers[(len(numbers)/2)-1]
+            index = int(len(numbers)/2)
+            num1 = numbers[index]
+            num2 = numbers[index-1]
             print((num1+num2)/2)
 
     except ValueError:
